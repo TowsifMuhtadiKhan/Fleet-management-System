8a825cd3d7270846d5e9376fead2bc8ce5cc40e1
TowsifMuhtadiKhan <towsifanroidmarket@gmail.com>
2023-10-17 20:30:33 +0600

    Login and Signup updated with database

---
+++ process_login.php
@@ -0,0 +1,41 @@
+<?php
+// Establish a MySQL database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "fms_db";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Start a session
+session_start();
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // You should perform data validation and sanitization here.
+
+    // Query the database to check if the username and password match
+    $sql = "SELECT * FROM users WHERE `Username`='$username' AND `Password`='$password'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Successful login
+        header("Location: userprofile.html");
+        exit();
+    } else {
+        // Login failed
+        echo "Login failed. Please check your username and password.";
+    }
+
+    $conn->close();
+}
+?>
---
+++ process_signup.php
@@ -0,0 +1,38 @@
+<?php
+// Establish a MySQL database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "fms_db";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $phone = $_POST["phone"];
+    $email = $_POST["email"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // You should perform data validation and sanitization here.
+
+    // Insert data into the database
+    $sql = "INSERT INTO users (`Name`, `Phone`, `Email`, `Username`, `Password`) VALUES ('$name', '$phone', '$email', '$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Successfully registered
+        echo "Registration successful!";
+    } else {
+        // Error occurred
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
