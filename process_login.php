1327532e0bf6a165949941879d9e438223fec532
Zahid Hasan <67813593+zhzahid1911509@users.noreply.github.com>
2023-10-17 20:56:46 +0600

    signup and signin updated

--- process_login.php
+++ process_login.php
@@ -1,16 +1,13 @@
 <?php
 // Establish a MySQL database connection
-<<<<<<< HEAD
+
+session_start();
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "fms_db";
-=======
-$servername = "your_mysql_server";
-$username = "your_mysql_username";
-$password = "your_mysql_password";
-$database = "your_database_name";
->>>>>>> bffab85a2da2befa23bec052c9c440146454dde2
+
 
 $conn = new mysqli($servername, $username, $password, $database);
 
@@ -30,21 +27,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // You should perform data validation and sanitization here.
 
     // Query the database to check if the username and password match
-<<<<<<< HEAD
+
     $sql = "SELECT * FROM users WHERE `Username`='$username' AND `Password`='$password'";
-=======
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
->>>>>>> bffab85a2da2befa23bec052c9c440146454dde2
+
+    
 
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // Successful login
-<<<<<<< HEAD
+
         header("Location: userprofile.html");
-=======
-        header("Location: dashboard.html");
->>>>>>> bffab85a2da2befa23bec052c9c440146454dde2
+
         exit();
     } else {
         // Login failed
--- process_signup.php
+++ process_signup.php
@@ -1,16 +1,11 @@
 <?php
 // Establish a MySQL database connection
-<<<<<<< HEAD
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "fms_db";
-=======
-$servername = "your_mysql_server";
-$username = "your_mysql_username";
-$password = "your_mysql_password";
-$database = "your_database_name";
->>>>>>> bffab85a2da2befa23bec052c9c440146454dde2
+
 
 $conn = new mysqli($servername, $username, $password, $database);
 
@@ -26,17 +21,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $username = $_POST["username"];
     $password = $_POST["password"];
+    $repeatPassword = $_POST["repeatPassword"];
 
     // You should perform data validation and sanitization here.
 
     // Insert data into the database
-<<<<<<< HEAD
-    $sql = "INSERT INTO users (`Name`, `Phone`, `Email`, `Username`, `Password`) VALUES ('$name', '$phone', '$email', '$username', '$password')";
-=======
-    $sql = "INSERT INTO users (name, phone, email, username, password) VALUES ('$name', '$phone', '$email', '$username', '$password')";
->>>>>>> bffab85a2da2befa23bec052c9c440146454dde2
 
-    if ($conn->query($sql) === TRUE) {
+    $sql = "INSERT INTO users (`Name`, `Phone`, `Email`, `Username`, `Password`) VALUES ('$name', '$phone', '$email', '$username', '$password')";
+
+    if($password !== $repeatPassword){
+        alert('Password not match');
+    }
+
+    else if ($conn->query($sql) === TRUE) {
         // Successfully registered
         echo "Registration successful!";
     } else {
