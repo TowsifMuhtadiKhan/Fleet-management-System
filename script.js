530438ee73cd46d453fa7f825da5d72994524649
TowsifMuhtadiKhan <towsifanroidmarket@gmail.com>
2023-10-17 12:51:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    //hamburger Toggle
+    $('.humbarger').click(function(event){
+    $('.menu-list').slideToggle(500);
+    event.preventDefault();
+    
+    $('.menu-list li a').click(function(event) {
+        if ($(window).width() < 768) {
+          $('.menu-list').slideUp(500);
+          event.preventDefault(); 
+        }
+      });
+    });
+    
+    });
